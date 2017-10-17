30c6d190d5b4d56f154395c18cf138f92944d2cd
Firas Chaaben <firaschaabencss@gmail.com>
2017-10-17 14:47:21 +0100

    method 2

--- display/views.py
+++ display/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
+"""
+method 1 :
 def index(request):
-    return HttpResponse("""<h1> hello </h1>
+    return HttpResponse('<h1> hello </h1>
     <button> send </button>
-    """)
+    ')
+"""    
+# method 2 : 
+def index(request):
+    return render(request,'index.html')
