f2b998ae558a72bae7a9d6f0b63ec812f3d40dcc
Firas Chaaben <firaschaabencss@gmail.com>
2017-10-20 12:36:13 +0100

    dynamique path

--- display/views.py
+++ display/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
-    return HttpResponse('hello')
+def index(request, name):
+    print(name)
+    return HttpResponse('hello '+name)
 """    
 # method 2 : 
 def index(request):
